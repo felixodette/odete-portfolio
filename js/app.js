1cd29a6861ed23c114f71f57b6b960fe06514f26
Felix Odete <felix.odette@gmail.com>
2022-04-11 15:17:25 +0300

    Mobile navigation

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const navSlide = () => {
+    const burger = document.querySelector('.menu-button');
+    const nav = document.querySelector('.desktop-nav');
+    const navLinks = document.querySelectorAll('.desktop-nav a');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        // navLinks.forEach(link, index) => {
+        //     if (link.style.animation) {
+        //         link.style.animation = '';
+        //     } else {
+        //         link.style.animation = `nav-links-fade 0.5s ease forwards $(index/7)s`;
+        //     }
+        // }
+    });
+}
+
+const app = () => {
+    navSlide();
+}
+
+app();
