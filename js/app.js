9cfc18831ab99e05c47764c1d248cfd5d006387f
Felix Odete <felix.odette@gmail.com>
2022-04-11 18:18:45 +0300

    Linter fixes

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const navSlide = () => {
   const nav = document.querySelector('.desktop-nav');
 
   burger.addEventListener('click', () => {
-  nav.classList.toggle('nav-active');
+    nav.classList.toggle('nav-active');
   });
 };
 
