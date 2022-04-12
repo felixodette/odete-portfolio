1bab0a0f3c554e81567b30ce0c8d416c6afe1241
Felix Odete <felix.odette@gmail.com>
2022-04-12 13:46:37 +0300

    Create modal for project

--- js/app.js
+++ js/app.js
@@ -7,6 +7,39 @@ const navSlide = () => {
   });
 };
 
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    scrollTop();
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+const scrollTop = () => window.scrollTo({ top: 0, behavior: 'smooth' })
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
 const app = () => {
   navSlide();
 };
