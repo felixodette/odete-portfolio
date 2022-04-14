cf01d4eecc9ae9e4e10d6b694792a76a7d7c2953
Felix Odete <felix.odette@gmail.com>
2022-04-14 22:43:15 +0300

    Eslint fixes

--- js/app.js
+++ js/app.js
@@ -7,9 +7,9 @@ const navSlide = () => {
   });
 };
 
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
 const projects = {
   name: 'Project name goes here',
   languages: ['HTML/CSS', 'Ruby on Rails', 'Javascript'],
@@ -18,44 +18,12 @@ const projects = {
   image_3: 'Rectangle%2040.svg',
   image_4: 'Rectangle%2041.svg',
   image_5: 'Rectangle%2042.svg',
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi\n' +
-    '                    <br>\n' +
-    '                    <br>\n' +
-    '                    Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi\n'
+    + '\n'
+    + 'Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
   live_link: '#',
-  repo: '#'
+  repo: '#',
 };
-
-openModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    scrollTop();
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
-  })
-})
-
-const scrollTop = () => window.scrollTo({ top: 0, behavior: 'smooth' })
-
-closeModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = button.closest('.modal')
-    closeModal(modal)
-  })
-})
-
-function openModal(modal) {
-  if (modal == null) return
-  modal.classList.add('active');
-  overlay.classList.add('active');
-  popup();
-}
-
-function closeModal(modal) {
-  if (modal == null) return
-  modal.classList.remove('active')
-  overlay.classList.remove('active')
-}
-
 const popup = () => {
   document.querySelector('[data-title]').innerHTML = projects.name;
   document.querySelector('#image-1').setAttribute('src', `../img/modals/${projects.image_1}`);
@@ -64,14 +32,43 @@ const popup = () => {
   document.querySelector('#image-4').setAttribute('src', `../img/modals/${projects.image_4}`);
   document.querySelector('#image-5').setAttribute('src', `../img/modals/${projects.image_5}`);
 
-  document.querySelector('[data-html-css]').textContent = projects.languages[0];
-  document.querySelector('[data-ruby-rails]').textContent = projects.languages[1];
-  document.querySelector('[data-javascript]').textContent = projects.languages[2];
+  const [htmlCss, rubyRails, jScript] = projects.languages;
+  document.querySelector('[data-html-css]').textContent = htmlCss;
+  document.querySelector('[data-ruby-rails]').textContent = rubyRails;
+  document.querySelector('[data-javascript]').textContent = jScript;
 
   document.querySelector('[data-description]').textContent = projects.description;
   document.querySelectorAll('[data-see-live]').forEach((alink) => alink.setAttribute('href', projects.live_link));
   document.querySelectorAll('[data-see-source]').forEach((alink) => alink.setAttribute('href', projects.repo));
+};
+const scrollTop = () => window.scrollTo({ top: 0, behavior: 'smooth' });
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add('active');
+  overlay.classList.add('active');
+  popup();
 }
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+}
+
+openModalButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    scrollTop();
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
+});
+
+closeModalButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal');
+    closeModal(modal);
+  });
+});
 
 const validateForm = (formItem) => {
   let invalid = 0;
@@ -116,6 +113,7 @@ const validateForm = (formItem) => {
 
 const app = () => {
   navSlide();
+  validateForm();
 };
 
 app();
